8caacb6d57399924c5c2113b04e846c56fcad554
Roshan Mykoo <roshanjmykoo@gmail.com>
2021-01-18 18:10:13 -0500

    first push of app
    removed default viewcontroller in story board, replaced with tabbed view with 2 new view controllers

---
+++ TaskMaster/TaskMaster/CompletedTasksViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CompletedTasksViewController.swift
+//  TaskMaster
+//
+//  Created by Roshan Mykoo on 1/18/21.
+//
+
+import Foundation
+import UIKit
+
+class CompletedTasksViewController: ViewController {
+   
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
---
+++ TaskMaster/TaskMaster/TasksViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  TaskMaster
+//
+//  Created by Roshan Mykoo on 1/18/21.
+//
+//loadView
+//viewDidLoad
+//viewWillAppear
+//viewWillLayoutSubviews
+//viewDidlayoutSubviews
+//viewDidAppear
+//viewWillAppear
+//viewDidDisappear
+//deinit
+//didReceiveMemoryWarning()
+//viewWillTransition(to:with:)
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
